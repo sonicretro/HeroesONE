e90f178041446326f4c5f848072ec71b47ecd0f8
MainMemory <sonicmike2@yahoo.com>
2014-05-11 15:27:47 -0500

    Initial Commit

---
+++ HeroesONE/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using FraGag.Compression;
+using HeroesONELib;
+
+namespace HeroesONE
+{
+    static class Program
+    {
+		static LongOpt[] opts = {
+							 new LongOpt("help", Argument.No, null, 'h'),
+							 new LongOpt("no-compression", Argument.No, null, 'n'),
+							 new LongOpt("pack", Argument.No, null, 'p'),
+							 new LongOpt("unpack", Argument.No, null, 'u')
+						 };
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+			Getopt getopt = new Getopt("KensSharp", args, Getopt.digest(opts), opts);
+			Mode? mode = null;
+			bool compress = true;
+			int opt = getopt.getopt();
+			while (opt != -1)
+			{
+				switch (opt)
+				{
+					case 'h':
+						ShowHelp();
+						return;
+					case 'p':
+						mode = Mode.Pack;
+						break;
+					case 'u':
+						mode = Mode.Unpack;
+						break;
+					case 'n':
+						compress = false;
+						break;
+				}
+				opt = getopt.getopt();
+			}
+			if (mode == null || getopt.Optind + (mode == Mode.Unpack ? 0 : 1) >= args.Length)
+			{
+				ShowHelp();
+				return;
+			}
+			string input = args[getopt.Optind];
+            switch (mode.Value)
+            {
+                case Mode.Unpack:
+                    try
+                    {
+                        HeroesONEFile one = new HeroesONEFile(input);
+                        string dest = Environment.CurrentDirectory;
+						if (getopt.Optind + 1 < args.Length)
+                        {
+                            dest = args[getopt.Optind + 1];
+                            Directory.CreateDirectory(dest);
+                        }
+						foreach (HeroesONEFile.File item in one.Files)
+							if (!compress)
+								File.WriteAllBytes(Path.Combine(dest, item.Name), item.Data);
+							else
+								File.WriteAllBytes(Path.Combine(dest, item.Name), Prs.Decompress(item.Data));
+					}
+                    catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+                    break;
+                case Mode.Pack:
+                    string fn = args[args.Length - 1];
+                    try
+                    {
+                        HeroesONEFile ar = new HeroesONEFile();
+                        for (int i = getopt.Optind; i < args.Length - 1; i++)
+                            ar.Files.Add(new HeroesONEFile.File(args[i]));
+                        ar.Save(fn);
+                    }
+                    catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+                    break;
+            }
+        }
+	
+		static void ShowHelp()
+		{
+			Console.Write(Properties.Resources.HelpText);
+		}
+	}
+
+	enum Mode
+	{
+		Pack,
+		Unpack
+	}
+}
---
+++ HeroesONELib/HeroesONEFile.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HeroesONELib
+{
+    public class HeroesONEFile
+    {
+        public class File
+        {
+            public string Name { get; set; }
+            public byte[] Data { get; set; }
+
+            public File()
+            {
+                Name = string.Empty;
+            }
+
+            public File(string fileName)
+            {
+                Name = Path.GetFileName(fileName);
+                Data = System.IO.File.ReadAllBytes(fileName);
+            }
+
+            public File(string name, byte[] data)
+            {
+                Name = name;
+                Data = data;
+            }
+        }
+
+        public List<File> Files { get; set; }
+
+        const int Magic = 0x1400FFFF;
+
+        public HeroesONEFile()
+        {
+            Files = new List<File>();
+        }
+
+        public HeroesONEFile(string filename)
+        {
+            Files = new List<File>();
+            using (FileStream stream = System.IO.File.OpenRead(filename))
+            using (BinaryReader reader = new BinaryReader(stream, System.Text.Encoding.ASCII))
+            {
+                stream.Seek(4, SeekOrigin.Current);
+                int filesize = reader.ReadInt32() + 0xC;
+                if (reader.ReadInt32() != Magic)
+                    throw new Exception("Error: Unknown archive type");
+                stream.Seek(4, SeekOrigin.Current);
+                long fnlength = reader.ReadInt32();
+                stream.Seek(4, SeekOrigin.Current);
+                List<string> filenames = new List<string>((int)(fnlength / 64));
+                fnlength += stream.Position;
+                while (stream.Position < fnlength)
+                    filenames.Add(reader.ReadString(64));
+                stream.Seek(fnlength, SeekOrigin.Begin);
+                while (stream.Position < filesize)
+                {
+                    int fn = reader.ReadInt32();
+                    int sz = reader.ReadInt32();
+                    stream.Seek(4, SeekOrigin.Current);
+                    Files.Add(new File(filenames[fn], reader.ReadBytes(sz)));
+                }
+            }
+        }
+
+        public void Save(string filename)
+        {
+            byte[] filenames = new byte[256 * 64];
+            using (FileStream stream = System.IO.File.Open(filename, FileMode.Create, FileAccess.Write))
+            using (BinaryWriter writer = new BinaryWriter(stream, System.Text.Encoding.ASCII))
+            {
+                writer.Write(0);
+                long fspos = stream.Position;
+                writer.Write(-1);
+                writer.Write(Magic);
+                long fsstart = stream.Position;
+                writer.Write(1);
+                writer.Write(filenames.Length);
+                writer.Write(Magic);
+                long fnpos = stream.Position;
+                writer.Write(filenames);
+                int i = 2;
+                foreach (File item in Files)
+                {
+                    writer.Write(i++);
+                    writer.Write(item.Data.Length);
+                    writer.Write(Magic);
+                    writer.Write(item.Data);
+                    System.Text.Encoding.ASCII.GetBytes(item.Name).CopyTo(filenames, i * 64);
+                }
+                stream.Seek(fnpos, SeekOrigin.Begin);
+                writer.Write(filenames);
+                stream.Seek(fspos, SeekOrigin.Begin);
+                writer.Write((int)(stream.Length - fsstart));
+            }
+        }
+
+        private static string GetCString(byte[] file, int address)
+        {
+            int textsize = 0;
+            while (file[address + textsize] > 0 & textsize < 64)
+                textsize++;
+            return Encoding.ASCII.GetString(file, address, textsize);
+        }
+    }
+    public static class Extensions
+    {
+        /// <summary>
+        /// Reads a null-terminated ASCII string from the current stream and advances the position by <paramref name="length"/> bytes.
+        /// </summary>
+        /// <param name="length">The maximum length of the string, in bytes.</param>
+        public static string ReadString(this BinaryReader br, int length)
+        {
+            byte[] buffer = br.ReadBytes(length);
+            for (int i = 0; i < length; i++)
+                if (buffer[i] == 0)
+                    return Encoding.ASCII.GetString(buffer, 0, i);
+            return Encoding.ASCII.GetString(buffer);
+        }
+    }
+}
