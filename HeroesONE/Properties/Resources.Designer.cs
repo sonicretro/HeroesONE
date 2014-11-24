45b2e761c00997f4528a045b4757fe7722a7ae57
MainMemory <sonicmike2@yahoo.com>
2014-11-24 13:07:02 -0600

    What even is going on here

--- HeroesONE/Properties/Resources.Designer.cs
+++ HeroesONE/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace HeroesONE.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SonicRetro.KensSharp.KensSharp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HeroesONE.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
